a66859b5e32b52269dc93445c9b2378f02f8bb0e
litvi <litvin4uk.roman@gmail.com>
2025-09-10 19:31:13 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -6,12 +6,15 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
+
+# Read SSH key
 $sshKeyPath = Join-Path $HOME ".ssh/id_ed25519.pub"
-$sshKeyPublicKey = $null
-if (Test-Path $sshKeyPath) {
-    $sshKeyPublicKey = (Get-Content -Raw $sshKeyPath).Trim()
+if (-not (Test-Path $sshKeyPath)) {
+    Write-Error "SSH public key not found at $sshKeyPath. Please generate or specify a valid key before running this script."
+    exit 1
 }
-$vmName = "matebox"
+$sshKeyPublicKey = (Get-Content -Raw $sshKeyPath).Trim()
+
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
 
@@ -19,8 +22,8 @@ Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
@@ -28,17 +31,14 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-# Take a note that in this task VMs are deployed without public IPs and you won't be able
-# to connect to them - that's on purpose! The "free" Public IP resource (Basic SKU,
-# dynamic IP allocation) can't be deployed to the availability zone, and therefore can't 
-# be attached to the VM. Don't trust me - test it yourself! 
-# If you want to get a VM with public IP deployed to the availability zone - you need to use 
-# Standard public IP SKU (which you will need to pay for, it is not included in the free account)
-# and set same zone you would set on the VM, but this is not required in this task. 
-# New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel "random32987"
-
-$zones = @(1, 2)
+# Two VMs in different availability zones
+$zones = @('1','2')
 $vmNames = @("matebox1", "matebox2")
+
+if ($zones.Length -ne $vmNames.Length) {
+    Write-Error "Zones and VM names arrays must be the same length."
+    exit 1
+}
 
 for ($i = 0; $i -lt $zones.Length; $i++) {
     Write-Host "Creating VM $($vmNames[$i]) in Zone $($zones[$i]) ..."
